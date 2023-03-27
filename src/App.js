8e1bc945fa5c859988a9c8cb165e0ac109f13f89
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2023-03-27 19:47:53 +0200

    Add App Functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      // todo: todo,
+      todo,
+    };
+    // add todo to the list
+    setList([...list, newTodo]);
+    // clear input box
+    setInput('');
+  };
+  // delete todo
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
   return (
-    <div className="App">
-      <h1>React Todo App</h1>
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button type="button" onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button type="button" onClick={() => deleteTodo(todo.id)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
